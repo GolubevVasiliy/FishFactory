4af3c0909a0545a525810b2d2fa62c67a06bd593
Vasiliy <mixfruit9@gmail.com>
2024-01-31 00:10:06 +0300

    looks like I set it up with navmesh

---
+++ Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMovement : MonoBehaviour
+{
+    private Vector3 _targetPositions;
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;
+        _agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        SetTargetPositions();
+        SetAgentPosition();
+    }
+
+    private void SetTargetPositions()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("1");
+            _targetPositions = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void SetAgentPosition()
+    {
+        Debug.Log("2");
+        _agent.SetDestination(new Vector3(_targetPositions.x, _targetPositions.y, transform.position.z));
+    }
+}
